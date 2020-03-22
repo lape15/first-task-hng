28f9452b1d471dc8d5d15afb0d953f690fed7ac8
Lape15 <larape1993@gmail.com>
2020-03-22 15:08:19 +0100

    javascript-task

---
+++ script.js
@@ -0,0 +1,7 @@
+const name  = 'Lape'
+const courses = ['html','css','javaScript','Node js', 'Flutter']
+for (let i = 0; i  <= 200; i++){
+    if((i % 2) === 1) {
+        console.log(i, 'is odd')
+    }
+}
